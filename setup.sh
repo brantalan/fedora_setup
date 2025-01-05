a3e2d6ba02e04001bfb1467279754d4f3619dc19
BrantAlan <github@wilmo.email>
2025-01-04 22:36:57 -0500

    separating yaru theme from apps

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Run Yaru Theme setup
+bash yaru-theme.sh
+
+# Run Applications setup
+bash applications.sh
+
--- yaru-theme.sh
+++ yaru-theme.sh
@@ -1,24 +1,7 @@
 #!/bin/bash
 
-# Update system packages
-sudo dnf update -y
-
-# Install additional packages
-sudo dnf install -y gnome-tweaks gnome-console yaru-theme fastfetch
-
-# Remove RPM-based Firefox
-sudo dnf remove -y firefox ptyxis
-
-# Add the Flathub repository to Flatpak
-sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-
-# Install Flatpak applications from Flathub
-sudo flatpak install -y flathub org.mozilla.firefox
-sudo flatpak install -y flathub org.mozilla.Thunderbird
-sudo flatpak install -y flathub com.github.neithern.g4music
-sudo flatpak install -y flathub de.haeckerfelix.Fragments
-sudo flatpak install -y flathub io.ente.auth
-sudo flatpak install -y flathub com.mattjakeman.ExtensionManager
+# Install Yaru theme
+sudo dnf install -y yaru-theme
 
 # Install Ubuntu Sans and Ubuntu Sans Mono fonts
 FONT_DIR="/usr/share/fonts/google-fonts"
@@ -71,24 +54,4 @@ gsettings set org.gnome.desktop.interface icon-theme 'Yaru'
 gsettings set org.gnome.desktop.interface cursor-theme 'Yaru'
 gsettings set org.gnome.desktop.sound theme-name 'Yaru'
 gsettings set org.gnome.desktop.wm.preferences theme 'Yaru'
-
-# Install Proton VPN and related components
-# Download and add Proton VPN repository
-wget "https://repo.protonvpn.com/fedora-$(cat /etc/fedora-release | cut -d' ' -f 3)-stable/protonvpn-stable-release/protonvpn-stable-release-1.0.2-1.noarch.rpm"
-sudo rpm --import https://repo.protonvpn.com/fedora-$(cat /etc/fedora-release | cut -d' ' -f 3)-stable/public_key.asc
-sudo dnf install -y ./protonvpn-stable-release-1.0.2-1.noarch.rpm && sudo dnf check-update --refresh
-
-# Install Proton VPN GNOME Desktop app
-sudo dnf install -y proton-vpn-gnome-desktop
-
-# Install system tray icon support (optional for Proton VPN)
-sudo dnf install -y libappindicator-gtk3
-
-# Install Proton Bridge
-wget "https://proton.me/download/bridge/protonmail-bridge-3.13.0-1.x86_64.rpm"
-sudo dnf install -y ./protonmail-bridge-3.13.0-1.x86_64.rpm
-
-# Install Proton Pass
-wget "https://proton.me/download/PassDesktop/linux/x64/ProtonPass.rpm"
-sudo dnf install -y ./ProtonPass.rpm
 
